9321cc134decb80498a6c4ec1a41ead68f84adf8
Luke Ponga <developmentdesignsltd@gmail.com>
2025-11-07 23:42:45 +0000

    Get data from https://trafficnz.info/service/traffic/rest/4/cameras/all

--- .next/static/chunks/src_app_page_tsx_1c86e4e6._.js
+++ .next/static/chunks/src_app_page_tsx_1c86e4e6._.js
@@ -1,9 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/src_app_globals_b805903d.css",
-  "static/chunks/node_modules_5c40a569._.js",
-  "static/chunks/src_1dab6f22._.js"
+  "static/chunks/_153ca0b5._.js",
+  "static/chunks/node_modules_2144484a._.js"
 ],
     source: "dynamic"
 });
